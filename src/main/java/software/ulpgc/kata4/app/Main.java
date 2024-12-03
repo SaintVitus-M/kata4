9ea7c51fa97ec924f9eb1d3993a209bcc7b4f77e
Vit <vit@email.com>
2024-12-03 13:24:21 +0000

    Remove args from main method.

--- src/main/java/software/ulpgc/kata4/app/Main.java
+++ src/main/java/software/ulpgc/kata4/app/Main.java
@@ -4,7 +4,7 @@ import software.ulpgc.kata4.architecture.control.Command;
 import software.ulpgc.kata4.architecture.control.DisplayCommand;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(){
         Command command = new DisplayCommand();
         command.execute();
     }
